e09d3dfbe73c4a019fdb1f2e8ded2c7853ef165a
Tianzhou <t@bytebase.com>
2026-02-16 08:45:05 -0800

    chore: update website header

--- website/src/lib/links.ts
+++ website/src/lib/links.ts
@@ -3,4 +3,5 @@ export const LINKS = {
   quickstart: 'https://docs.pgconsole.com/getting-started/quickstart',
   demo: 'https://demo.pgconsole.com',
   contact: 'mailto:dev@pgconsole.com',
+  github: 'https://github.com/pgplex/pgconsole',
 } as const
